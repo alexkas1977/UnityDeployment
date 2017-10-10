9c0d817652712c46b7b944d7eec26fcf6417fa56
Alexey Nikulin <alexey.nikulin@vixtera.com>
2017-10-10 12:49:30 +0300

    Switched to a local build media

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-curl -o /tmp/unity.sh http://nexus.cmlatitude.local:8081/nexus/content/sites/cl-p3c-artifacts/unity.sh
+#curl -o /tmp/unity.sh http://nexus.cmlatitude.local:8081/nexus/content/sites/cl-p3c-artifacts/unity.sh
 #curl -o /tmp/unity.sh http://build:QAZwsx123@teamcity.cmlatitude.com/repository/download/bt4/$BUILD:id/unity.sh
 /bin/bash /tmp/unity.sh -q -varfile /tmp/unity.properties -Vvalidation.skip=true 2>&1 
 chown -R mysql:mysql /var/lib/mysql/
